43b60bb6dbc3196f89db8cdeb0f45eb7bebec282
RivkyShachar <rvider@g.jct.ac.il>
2023-05-09 22:27:06 +0300

    add member is working in mvc

--- HelthFundMVC/Controllers/MembersController.cs
+++ HelthFundMVC/Controllers/MembersController.cs
@@ -25,7 +25,7 @@ namespace HelthFundMVC.Controllers
             _logger = logger;
         }
 
-        public async Task<IActionResult> GetAllMembers()
+        public async Task<IActionResult> GetAllMembersGet()
         {
             List<Member> members= new List<Member>();
             using (var httpClient = new HttpClient())
@@ -50,7 +50,47 @@ namespace HelthFundMVC.Controllers
             }
             return View();
         }
-        
+
+        public IActionResult AddMemberGet()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AddMemberPost(Member member)
+        {
+            Member obj = new Member()
+            {
+                Id = member.Id,
+                FirstName = member.FirstName, 
+                LastName = member.LastName,
+                Address = member.Address,
+                PhoneNumber = member.PhoneNumber,
+                MobileNumber = member.MobileNumber,
+                BirthDate = member.BirthDate,
+                ImageUrl = member.ImageUrl,
+            };
+            if(member.Id != null)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(baseURL);
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    var response = await httpClient.PostAsJsonAsync<Member>("/api/Members/AddMember", obj);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("GetAllMembersGet", "Members");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling web API");
+                    }
+                }
+            }
+            return View();
+        }
+
+
 
     }
 }
