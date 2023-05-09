ab92e03ef072e1c2b145ff52b608e11b9cbf6880
RivkyShachar <rvider@g.jct.ac.il>
2023-05-10 00:02:34 +0300

    add controllers to mvc

---
+++ HelthFundMVC/Controllers/RecoveriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelthFundMVC.Controllers
+{
+    public class RecoveriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HelthFundMVC/Controllers/VaccinesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelthFundMVC.Controllers
+{
+    public class VaccinesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
