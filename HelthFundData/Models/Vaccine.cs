8720da7387b81d2cdae23387dd2d951487ae336b
RivkyShachar <rvider@g.jct.ac.il>
2023-05-10 15:38:27 +0300

    set the butoons of everything

--- HelthFundData/Models/Vaccine.cs
+++ HelthFundData/Models/Vaccine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace HelthFundData.Models
     public class Vaccine
     {
         public int Id { get; set; }
+        [DisplayName("Member id")]
         public int MemberId { get; set; }
+        [DisplayName("Vaccine manufacturer")]
         public string VaccineManufacturer { get; set; }
+        [DisplayName("Vaccine date")]
         public DateTime VaccineDate { get; set; }
     }
 }
